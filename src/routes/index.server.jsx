25c08a543495b6cf8d6a8e749296239ef57a15f9
johncraigcole <johncraigcole@gmail.com>
2022-08-13 09:53:43 -0400

    add product load

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import { useShopQuery, gql, CacheNone } from "@shopify/hydrogen";
+
+export default function Home({ response }) {
+  // Disable page level caching
+  response.cache(CacheNone());
+
+  // Load a product
+  const {
+    data: { product },
+  } = useShopQuery({
+    query: gql`
+      query getAurora {
+        product(handle: "shopify-aurora") {
+          title
+          description
+        }
+      }
+    `,
+  });
+
+  return (
+    <>
+      <h1>{product.title}</h1>
+      <h3>{product.description}</h3>
+    </>
+  );
 }
