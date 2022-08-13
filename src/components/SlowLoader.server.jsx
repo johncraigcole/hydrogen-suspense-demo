e9db3072cea1dd6f628562ac63699ad146209b49
johncraigcole <johncraigcole@gmail.com>
2022-08-13 11:00:09 -0400

    adding suspense

--- src/components/SlowLoader.server.jsx
+++ src/components/SlowLoader.server.jsx
@@ -1,13 +1,11 @@
 import { useQuery } from "@shopify/hydrogen";
 export default function SlowLoader({ delayInMilliseconds }) {
   useQuery(
-    `no-key`,
+    `key-${delayInMilliseconds}`,
     () => new Promise((resolve) => setTimeout(resolve, delayInMilliseconds)),
     { cache: { mode: "no-store" } }
   );
   return (
-    <span>{`Delayed Request of ${
-      delayInMilliseconds / 1000
-    } seconds loaded.`}</span>
+    <span>{`Delayed Request of ${delayInMilliseconds} milliseconds loaded.`}</span>
   );
 }
--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,5 +1,6 @@
 import { useShopQuery, gql, CacheNone } from "@shopify/hydrogen";
 import SlowLoader from "../components/SlowLoader.server";
+import { Suspense } from "react";
 export default function Home({ response }) {
   // Disable page level caching
   response.cache(CacheNone());
@@ -18,13 +19,25 @@ export default function Home({ response }) {
     `,
   });
 
-  const delayTimesInMilliseconds = [10000, 3000, 1000];
+  const delayTimesInMilliseconds = [10000, 3000, 1000, 5000];
   return (
     <>
       <h1>{product.title}</h1>
       <h3>{product.description}</h3>
       {delayTimesInMilliseconds.map((itm) => {
-        return <SlowLoader delayInMilliseconds={itm} />;
+        return (
+          <Suspense
+            fallback={
+              <h4
+                style={{ padding: "10px", backgroundColor: "#BC8F8F" }}
+              >{`Loading a slow component delayed ${itm} milliseconds)`}</h4>
+            }
+          >
+            <h4 style={{ padding: "10px" }}>
+              <SlowLoader key={`sl-${itm}`} delayInMilliseconds={itm} />
+            </h4>
+          </Suspense>
+        );
       })}
     </>
   );
